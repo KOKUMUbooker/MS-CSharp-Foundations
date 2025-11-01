c9d152f2c5c98ef27e55541238167b874e4e15c6
KOKUMUbooker <bookerochieng@gmail.com>
2025-11-01 10:01:15 +0300

    Complete 'Modify-STR-Contents Challenge'

---
+++ 11.Modify-Str-Contents-Challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿// TARGET OUTPUT
+/*
+Quantity: 5000
+Output: <h2>Widgets &reg;</h2><span>5000</span>
+*/
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+// Quantity : 
+const string opSpan = "<span>";
+int openingSpanPos = input.IndexOf(opSpan);
+int closingSpanPos = input.IndexOf("</span>");
+
+openingSpanPos += opSpan.Length;
+int quantityStrLen = closingSpanPos - openingSpanPos;
+quantity = input.Substring(openingSpanPos, quantityStrLen);
+
+// Output : 
+output = input.Replace("&trade", "&reg");
+const string divOp = "<div>";
+int openingDivPos = output.IndexOf(divOp);
+int closingDivPos = output.IndexOf("</div>");
+
+openingDivPos += divOp.Length;
+int outputStrLen = closingDivPos - openingDivPos;
+output = output.Substring(openingDivPos,outputStrLen);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
