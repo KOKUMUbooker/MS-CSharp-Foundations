cbdec3f69cb5bc91c19b154d68f7588f9bc44055
KOKUMUbooker <bookerochieng@gmail.com>
2025-11-02 13:44:38 +0300

    Changes as a result of debugging tests using VS Code

--- 3.Conditionals-b/obj/Debug/net9.0/3.Conditionals-b.AssemblyInfo.cs
+++ 3.Conditionals-b/obj/Debug/net9.0/3.Conditionals-b.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3.Conditionals-b")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a953ad80d31e6fd35bd0becf097e54af919e0fbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("3.Conditionals-b")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3.Conditionals-b")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
